b3dfd64bfed65aaf552dc7e2a0c13a2f4fa43729
Carvallegro <valentin.carmignac@gmail.com>
2018-11-13 20:40:43 +1100

    Enriched Direction with logic

--- src/main/java/com/carvallegro/toyrobot/Coordinates.java
+++ src/main/java/com/carvallegro/toyrobot/Coordinates.java
@@ -1,11 +1,39 @@
 package com.carvallegro.toyrobot;
 
 public class Coordinates {
-    private int x;
-    private int y;
+    private Integer x;
+    private Integer y;
 
-    public Coordinates(int x, int y){
+    public Coordinates(Integer x, Integer y){
         this.x = x;
         this.y = y;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    @Override
+    protected Coordinates clone() throws CloneNotSupportedException {
+        return new Coordinates(x, y);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if(!(object instanceof Coordinates)){
+            return false;
+        }
+
+        Coordinates comparedTo = (Coordinates) object;
+        return x == comparedTo.x && y == comparedTo.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
 }
